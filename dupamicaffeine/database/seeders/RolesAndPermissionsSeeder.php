77214184c0fb5ef0fc8c67622176441d29d46508
mais <maismokayed2@gmail.com>
2025-08-15 21:50:31 +0300

    define system roles and permissions ,create RolesAndPermissionsSeeder

--- dupamicaffeine/database/seeders/RolesAndPermissionsSeeder.php
+++ dupamicaffeine/database/seeders/RolesAndPermissionsSeeder.php
@@ -2,16 +2,56 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use App\Enums\RoleEnum;
+use App\Enums\PermissionEnum;
 
 class RolesAndPermissionsSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+       
+        foreach (PermissionEnum::cases() as $permission) {
+            Permission::firstOrCreate([
+                'name' => $permission->value,
+                'guard_name' => 'web'
+            ]);
+        }
+
+        
+        foreach (RoleEnum::cases() as $role) {
+            Role::firstOrCreate([
+                'name' => $role->value,
+                'guard_name' => 'web'
+            ]);
+        }
+
+        
+        $adminRole = Role::where('name', RoleEnum::ADMIN->value)->first();
+        $adminRole->syncPermissions(Permission::all());
+
+    
+        $customerRole = Role::where('name', RoleEnum::CUSTOMER->value)->first();
+        $customerPermissions = [
+            PermissionEnum::VIEW_PRODUCTS->value,
+            PermissionEnum::VIEW_CATEGORIES->value,
+            PermissionEnum::ADD_TO_CART->value,
+            PermissionEnum::UPDATE_CART->value,
+            PermissionEnum::REMOVE_FROM_CART->value,
+            PermissionEnum::CHECKOUT->value,
+            PermissionEnum::VIEW_OWN_ORDERS->value,
+            PermissionEnum::APPLY_COUPON->value,
+            PermissionEnum::WRITE_REVIEW->value,
+            PermissionEnum::EDIT_OWN_REVIEW->value,
+            PermissionEnum::DELETE_OWN_REVIEW->value,
+            PermissionEnum::ADD_TO_WISHLIST->value,
+            PermissionEnum::REMOVE_FROM_WISHLIST->value,
+            PermissionEnum::VIEW_OWN_WISHLIST->value,
+            PermissionEnum::VIEW_OWN_PROFILE->value,
+            PermissionEnum::UPDATE_OWN_PROFILE->value
+        ];
+        $customerRole->syncPermissions($customerPermissions);
     }
 }
