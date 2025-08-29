cbea1414e9349c6065e8fb828814c333be92adb0
mais <maismokayed2@gmail.com>
2025-08-30 01:18:47 +0300

    create cart requuest(additem,updatequantity)

---
+++ dupamicaffeine/app/Http/Requests/AddItemCartRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AddItemCartRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+        
+            'cart_id' => 'required|exists:carts,id',
+            'product_id' => 'required|exists:products,id',
+            'quantity' => 'required|integer|min:1',
+        ];
+    }
+      public function messages()
+    {
+        return [
+            'cart_id.required' => 'معرّف العربة (cart_id) مطلوب',
+            'cart_id.exists' => 'العربة المحددة غير موجود',
+            'product_id.required' => 'معرّف المنتج (product_id) مطلوب',
+            'product_id.exists' => 'المنتج المحدد غير موجود',
+            'quantity.required' => 'الكمية مطلوبة',
+            'quantity.integer' => 'الكمية يجب أن تكون رقم صحيح',
+            'quantity.min' => 'الكمية يجب أن تكون 1 على الأقل',
+        ];
+    }
+}
