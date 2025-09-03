eaa1fbd58fabc891fd09d7303192889195400e72
mais <maismokayed2@gmail.com>
2025-09-03 21:34:31 +0300

    edit cart model

--- dupamicaffeine/app/Models/Cart.php
+++ dupamicaffeine/app/Models/Cart.php
@@ -9,6 +9,10 @@ class Cart extends Model
 {
     protected $fillable =['user_id'];
 
+     public function user() {
+        return $this->belongsTo(User::class);
+    }
+
 public function items()
     {
         return $this->hasMany(CartItem::class);
